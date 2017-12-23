6e96ac52c7e3f3ef559cefb2fc53334fdd51f9a3
Marcel Maas <maas.marcel@gmail.com>
2017-12-23 22:23:14 +0100

    Added the replace_env script

---
+++ s2i/bin/replace_env.py
@@ -0,0 +1,21 @@
+import os
+import fileinput
+from tempfile import mkstemp
+from shutil import move
+from os import fdopen, remove
+
+var_mask = "env_"
+for key in os.environ.keys():
+    if key.lower().startswith(var_mask):
+        file_path = "/opt/app-root/etc/nginx.d/module.conf"
+
+        #Create temp file
+        fh, abs_path = mkstemp()
+        with fdopen(fh,'w') as new_file:
+            with open(file_path) as old_file:
+                for line in old_file:
+                    new_file.write(line.replace("${"+key+"}", os.environ[key]))
+        #Remove original file
+        remove(file_path)
+        #Move new file
+        move(abs_path, file_path)
